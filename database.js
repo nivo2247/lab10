f2c640a5c034b7230a976c7e159466e242ae5352
Nick <nivo2247@gmail.com>
2018-11-09 21:58:51 -0700

    These instructions suck

--- database.js
+++ database.js
@@ -1,14 +1,14 @@
 var pgp = require('pg-promise')();
 var dbConfig = process.env.DATABASE_URL;
 
-const dbConfig = {
+/*const dbConfig = {
    host: 'localhost',
    port: 5432,
    database: 'lab6',
    user: 'postgres',
    password: 'postgres' // TODO: Fill in your PostgreSQL password here.
                 // Use empty string if you did not set a password
-};
+};*/
 
 var db = pgp(dbConfig);
 
