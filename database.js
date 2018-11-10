718b42475378e7ff6baf05499181314b441f51f9
Nick <nivo2247@gmail.com>
2018-11-09 21:51:18 -0700

    Second try

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 var pgp = require('pg-promise')();
 var dbConfig = process.env.DATABASE_URL;
-/*
+
 const dbConfig = {
    host: 'localhost',
    port: 5432,
@@ -9,7 +9,7 @@ const dbConfig = {
    password: 'postgres' // TODO: Fill in your PostgreSQL password here.
                 // Use empty string if you did not set a password
 };
-*/
+
 var db = pgp(dbConfig);
 
 module.exports = db;
